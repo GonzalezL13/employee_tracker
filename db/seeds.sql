be5bf1153060410daf810a9a30f42054e62ed641
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-08-15 14:05:21 -0700

    seed.sql created

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+use employees;
+
+
+INSERT INTO department (name)
+VALUES ('Back Office'),
+    ('Front Office'),
+    ('Call Center');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Medical Assistant Lead', 36000, 1),
+    ('Medical Assistant', 32000, 1),
+    ('Front Desk Lead', 33000, 2),
+    ('Front Desk', 30000, 2),
+    ('Call Center Lead', 33000, 3),
+    ('Scheduler', 30000, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Christina', 'Salas', 1, NULL),
+    ('Roger', 'Garcia', 2, 1),
+    ('Kayla', 'Pfeiff', 3, NULL),
+    ('Cassy', 'Perrow', 4, 3),
+    ('Alfredo', 'Vale', 5, NULL),
+    ('Alex', 'Brown', 6, 5);
